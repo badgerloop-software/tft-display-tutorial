dc764ae192c0b77b71fbe365c4a9ab12d8032997
Porter Fencl <pfencl@wisc.edu>
2025-09-29 20:44:38 -0500

    finished task 2

--- src/display.cpp
+++ src/display.cpp
@@ -3,8 +3,21 @@
 //Initialize the display such that:
 //It is horizontally oriented
 //The display color is initialized to orange
-void initDisplay(){}
+TFT_eSPI tft = TFT_eSPI();
+void initDisplay(){
+    tft.begin();
+    tft.setRotation(1);
+    tft.fillScreen(0xFD20);
+}
 
-void setDisplayColor(uint16_t color){}
+void setDisplayColor(uint16_t color){
+    tft.fillScreen(color);
+}
+uint16_t colors[] = {TFT_RED, TFT_GREEN, TFT_BLUE, TFT_YELLOW, TFT_CYAN, TFT_MAGENTA};
+void cycleDisplayColor(){
 
-void cycleDisplayColor(){}
+    static uint8_t colorIndex = 0;
+
+    tft.fillScreen(colors[colorIndex]);
+    colorIndex = (colorIndex + 1) % (sizeof(colors) / sizeof(colors[0]));
+}
